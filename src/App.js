616bddcb6da3d22f71d649ae47be14e1dcb29f9c
sujilals <44774289+sujilals@users.noreply.github.com>
2020-01-09 17:49:44 +0530

    Search bar Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+// CSS and Js
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
 
-function App() {
+// Components
+import SearchBar from "./components/layouts/SearchBar";
+
+const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
     </div>
   );
-}
+};
 
 export default App;
