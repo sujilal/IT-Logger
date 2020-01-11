e2885b56b26f311e05eb3047b2e4435461d0a3f6
sujilals <44774289+sujilals@users.noreply.github.com>
2020-01-11 16:14:56 +0530

    getlogs using redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,2 +1,62 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
+// Method1 for GETLOGS
+
+// export const getLogs = () => {
+//   return async dispatch => {
+//     setLoading();
+//     const res = await fetch("/logs");
+//     const data = await res.data;
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data
+//     });
+//   };
+// };
+
+// Method 2 Get logs from Server
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// Method 2 Add logs from Server
+export const addLogs = () => async dispatch => {
+  try {
+    setLoading();
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
+
+export const logErrors = () => {};
